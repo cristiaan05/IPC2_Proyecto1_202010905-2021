cc9a979703e137230edbb93434a5a0f61436ff50
Cristian Hernández <chtello05@gmail.com>
2021-08-09 19:00:08 -0600

    first release

---
+++ main.py
@@ -0,0 +1,28 @@
+if __name__ == '__main__':
+    print("Menu Principal: ")
+    print("     1. Cargar Archivo")
+    print("     2. Procesar Archivo")
+    print("     3. Escribir Archivo de Salida")
+    print("     4. Mostrar Datos del Estudiante")
+    print("     5. Generar Grafica")
+    print("     6. Salida")
+    opcion=input("Ingrese la opcion que desea:  ")
+    
+    if opcion=="1":
+        print("Ingrese la ruta del archivo: ")
+    elif opcion=="2":
+        print("-Calculando la mejor ruta")
+        print("-Calculando cantidad de combustible")
+    elif opcion=="3":
+        print("Escribir una ruta especifica: ")
+    elif opcion=="4":
+        print("Cristian Hernandez")
+        print("202010905")
+        print("Introduccion a la Programacion y Computacion 1 E")
+        print("Ingenieria en Ciencias y Sistemas")
+        print("4to Semestre")
+                
+        
+        
+        
+    
